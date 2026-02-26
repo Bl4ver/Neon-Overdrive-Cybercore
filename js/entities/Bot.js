828114fc8fc7f383899887a26de091667116d242
Bl4ver <bazsopatrik07@gmail.com>
2026-02-26 18:29:05 +0100

    a

--- js/entities/Bot.js
+++
@@ -1,8 +0,0 @@
-class Bot {
-    constructor(config) {
-        this.id = config.id;
-        this.slots = config.slots; // Hány fegyverhelye van
-        this.weapons = [];         // Ide kerülnek a Weapon példányok
-        this.aiLevel = config.ai;  // Mennyire okos
-    }
-}
